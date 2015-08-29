e9caf6399754dfab9f9a2e024a95afb5fea84bfc
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2015-08-29 16:39:39 +0000

    Initial import new project djunits.

---
+++ trunk/djunits/src/main/java/org/djunits/unit/unitsystem/AU.java
@@ -0,0 +1,28 @@
+package org.djunits.unit.unitsystem;
+
+/**
+ * The atomic unit system.
+ * <p>
+ * Copyright (c) 2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * <p>
+ * $LastChangedDate: 2015-07-26 01:01:13 +0200 (Sun, 26 Jul 2015) $, @version $Revision: 1155 $, by $Author: averbraeck $,
+ * initial version Jun 6, 2014 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class AU extends UnitSystem
+{
+    /** */
+    private static final long serialVersionUID = 20140607L;
+
+    /**
+     * protected constructor to avoid creating other (false) unit systems.
+     * @param abbreviationKey the abbreviation of the unit system, such as SI
+     * @param nameKey the name of the unit system, such as SI Base
+     */
+    protected AU(final String abbreviationKey, final String nameKey)
+    {
+        super(abbreviationKey, nameKey);
+    }
+
+}
