d4c4dcdea3c3fbe6e301e6cc41e5d1082b424f48
OTSim <P.Knoppers@tudelft.nl>
2015-09-27 11:48:41 +0000

    Wrote unit tests that cover all (?) private no-argument constructors. Wrote unit tests for ValueException and UnitException.

--- djunits/src/test/java/org/djunits/unit/UnitTest.java
+++ djunits/src/test/java/org/djunits/unit/UnitTest.java
@@ -62,4 +62,61 @@ public class UnitTest
             }
         }
     }
+    
+    /**
+     * Test all constructors for UnitException.
+     */
+    @Test
+    public void unitExceptionTest()
+    {
+        String message = "MessageString";
+        Exception e = new UnitException(message);
+        assertTrue("Exception should not be null", null != e);
+        assertEquals("message should be our message", message, e.getMessage());
+        assertEquals("cause should be null", null, e.getCause());
+        e = new UnitException();
+        assertTrue("Exception should not be null", null != e);
+        assertEquals("cause should be null", null, e.getCause());
+        String causeString = "CauseString";
+        Throwable cause = new Throwable(causeString);
+        e = new UnitException(cause);
+        assertTrue("Exception should not be null", null != e);
+        assertEquals("cause should not be our cause", cause, e.getCause());
+        assertEquals("cause description should be our cause string", causeString, e.getCause().getMessage());
+        e = new UnitException(message, cause);
+        assertTrue("Exception should not be null", null != e);
+        assertEquals("message should be our message", message, e.getMessage());
+        assertEquals("cause should not be our cause", cause, e.getCause());
+        assertEquals("cause description should be our cause string", causeString, e.getCause().getMessage());
+        for (boolean enableSuppression : new boolean[] { true, false })
+        {
+            for (boolean writableStackTrace : new boolean[] { true, false })
+            {
+                e = new UnitException(message, cause, enableSuppression, writableStackTrace);
+                assertTrue("Exception should not be null", null != e);
+                assertEquals("message should be our message", message, e.getMessage());
+                assertEquals("cause should not be our cause", cause, e.getCause());
+                assertEquals("cause description should be our cause string", causeString, e.getCause().getMessage());
+                // Don't know how to check if suppression is enabled/disabled
+                StackTraceElement[] stackTrace = new StackTraceElement[1];
+                stackTrace[0] = new StackTraceElement("a", "b", "c", 1234);
+                try
+                {
+                    e.setStackTrace(stackTrace);
+                }
+                catch (Exception e1)
+                {
+                    assertTrue("Stack trace should be writable", writableStackTrace);
+                    continue;
+                }
+                // You wouldn't believe it, but a call to setStackTrace if non-writable is silently ignored
+                StackTraceElement[] retrievedStackTrace = e.getStackTrace();
+                if (retrievedStackTrace.length > 0)
+                {
+                    assertTrue("stack trace should be writable", writableStackTrace);
+                }
+            }
+        }
+    }
+    
 }
