58acfca3096db7565c8a9dcbf9d8b435d66e6277
OTSim <P.Knoppers@tudelft.nl>
2015-09-29 12:16:31 +0000

    Enabling svn keywords LastChangedDate Revision Author Id

--- djunits-generator/src/main/java/code/generators/CodeGenerator.java
+++ djunits-generator/src/main/java/code/generators/CodeGenerator.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
  * <p>
- * $LastChangedDate: 2015-07-26 01:01:13 +0200 (Sun, 26 Jul 2015) $, @version $Revision: 1155 $, by $Author: pknoppers
+ * $LastChangedDate$, @version $Revision$, by $Author: pknoppers
  * $, initial version okt. 2014 <br>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  */
--- djunits-generator/src/main/java/code/generators/MathFunction.java
+++ djunits-generator/src/main/java/code/generators/MathFunction.java
@@ -7,7 +7,7 @@ package code.generators;
  * reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
  * <p>
- * $LastChangedDate: 2015-07-26 01:01:13 +0200 (Sun, 26 Jul 2015) $, @version $Revision: 1155 $, by $Author: pknoppers
+ * $LastChangedDate$, @version $Revision$, by $Author: pknoppers
  * $, initial version 0 sep. 2014 <br>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  */
