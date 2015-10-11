fbce621f08064b5b9bdfc448751e300c6fef0d9f
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2015-10-11 23:08:33 +0000

    Updated most tools in Maven to the latest version.

--- trunk/djunits/src/main/java/org/djunits/value/formatter/Formatter.java
+++ trunk/djunits/src/main/java/org/djunits/value/formatter/Formatter.java
@@ -1,6 +1,7 @@
 package org.djunits.value.formatter;
 
 /**
+ * Formatter of values with width and precision.
  * <p>
  * Copyright (c) 2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="http://djunits.org/docs/license.html">DJUNITS License</a>.
