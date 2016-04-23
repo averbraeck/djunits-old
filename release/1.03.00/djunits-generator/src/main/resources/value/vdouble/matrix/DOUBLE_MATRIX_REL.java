d379d20013fc3db7813081d6e871220e9464750a
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2016-04-23 19:53:13 +0000

    Version 1.03.00. The following changes have been made: Time.Abs, Time.Rel               --> Time, Duration (TimeUnit) Length.Abs, Length.Rel           --> Position, Length (LengthUnit) Temperature.Abs, Temperature.Rel --> AbsoluteTemperature, Temperature (TemperatureUnit) Angle.Abs, Angle.Rel             --> Direction, Angle (AngleUnit)

---
+++ release/1.03.00/djunits-generator/src/main/resources/value/vdouble/matrix/DOUBLE_MATRIX_REL.java
@@ -0,0 +1,86 @@
+package org.djunits.value.vdouble.matrix;
+
+import java.util.List;
+import java.util.SortedMap;
+
+import org.djunits.unit.*;
+import org.djunits.value.StorageType;
+import org.djunits.value.ValueException;
+import org.djunits.value.vdouble.scalar.*;
+
+/**
+ * Immutable Double %Type%Matrix, a matrix of values with a %Type%Unit. 
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Oct 9, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class %Type%Matrix extends
+    TypedDoubleMatrixRel<%Type%Unit, %Type%Matrix, Mutable%Type%Matrix, %Type%>
+{
+    /** */
+    private static final long serialVersionUID = 20151109L;
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Matrix.
+     * @param values double[][]; the values of the entries in the new Relative Immutable Double %Type%Matrix
+     * @param unit U; the unit of the new Relative Immutable Double %Type%Matrix
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public %Type%Matrix(final double[][] values, final %Type%Unit unit, final StorageType storageType)
+        throws ValueException
+    {
+        super(values, unit, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Matrix.
+     * @param values DoubleScalar.Rel&lt;U&gt;[][]; the values of the entries in the new Relative Immutable Double
+     *            %Type%Matrix
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public %Type%Matrix(final %Type%[][] values, final StorageType storageType) throws ValueException
+    {
+        super(values, storageType);
+    }
+
+    /**
+     * @param data an internal data object
+     * @param unit the unit
+     */
+    %Type%Matrix(final DoubleMatrixData data, final %Type%Unit unit)
+    {
+        super(data, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final %Type%Matrix instantiateType(final DoubleMatrixData dmd, final %Type%Unit unit)
+    {
+        return new %Type%Matrix(dmd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Mutable%Type%Matrix instantiateMutableType(final DoubleMatrixData dmd, final %Type%Unit unit)
+    {
+        return new Mutable%Type%Matrix(dmd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final %Type% get(final int row, final int column) throws ValueException
+    {
+        return new %Type%(getInUnit(row, column, getUnit()), getUnit());
+    }
+
+    
+%FORMULAS%%Type%%
+}
+
---
+++ release/1.03.00/djunits-generator/src/main/resources/value/vfloat/matrix/FLOAT_MATRIX_AR_ABS.java
@@ -0,0 +1,100 @@
+package org.djunits.value.vfloat.matrix;
+
+import java.util.List;
+import java.util.SortedMap;
+
+import org.djunits.unit.*;
+import org.djunits.value.StorageType;
+import org.djunits.value.ValueException;
+import org.djunits.value.vfloat.scalar.*;
+
+/**
+ * Immutable Float%TypeAbs% Matrix.
+ * <p>
+ * Copyright (c) 2013-2015 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://djunits.org/docs/license.html">DJUNITS License</a>.
+ * <p>
+ * $LastChangedDate: 2015-09-29 14:14:28 +0200 (Tue, 29 Sep 2015) $, @version $Revision: 73 $, by $Author: pknoppers $, initial
+ * version Sep 5, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class Float%TypeAbs%Matrix extends
+        TypedFloatMatrixAbs<%TypeUnit%, Float%TypeAbs%Matrix, Float%TypeRel%Matrix, MutableFloat%TypeAbs%Matrix, Float%TypeAbs%>
+{
+    /** */
+    private static final long serialVersionUID = 20151003L;
+
+    /**
+     * Construct a new Absolute Immutable Float%TypeAbs%Matrix.
+     * @param values float[][]; the values of the entries in the new Absolute Immutable Float%TypeAbs%Matrix
+     * @param unit U; the unit of the new Absolute Immutable Float%TypeAbs%Matrix
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public Float%TypeAbs%Matrix(final float[][] values, final %TypeUnit% unit, final StorageType storageType) throws ValueException
+    {
+        super(values, unit, storageType);
+    }
+
+    /**
+     * Construct a new Absolute Immutable Float%TypeAbs%Matrix.
+     * @param values FloatScalar.Rel&lt;U&gt;[][]; the values of the entries in the new Absolute Immutable Float%TypeAbs%Matrix
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public Float%TypeAbs%Matrix(final Float%TypeAbs%[][] values, final StorageType storageType) throws ValueException
+    {
+        super(values, storageType);
+    }
+
+    /**
+     * Construct a new Absolute Immutable Float%TypeAbs%Matrix.
+     * @param data an internal data object
+     * @param unit the unit
+     */
+    Float%TypeAbs%Matrix(final FloatMatrixData data, final %TypeUnit% unit)
+    {
+        super(data, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Float%TypeAbs%Matrix instantiateTypeAbs(final FloatMatrixData fmd, final %TypeUnit% unit)
+    {
+        return new Float%TypeAbs%Matrix(fmd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Float%TypeRel%Matrix instantiateTypeRel(final FloatMatrixData fmd, final %TypeUnit% unit)
+    {
+        return new Float%TypeRel%Matrix(fmd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final MutableFloat%TypeAbs%Matrix
+        instantiateMutableType(final FloatMatrixData fmd, final %TypeUnit% unit)
+    {
+        return new MutableFloat%TypeAbs%Matrix(fmd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final Float%TypeAbs% get(final int row, final int column) throws ValueException
+    {
+        return new Float%TypeAbs%(getInUnit(row, column, getUnit()), getUnit());
+    }
+    
+    /**
+     * Translate the absolute matrix into a relative matrix (e.g., before or after a multiplication or division).
+     * @return a relative version of this absolute Float%TypeAbs% matrix.
+     */
+    public final Float%TypeRel%Matrix toRel()
+    {
+        return new Float%TypeRel%Matrix(getData(), getUnit());
+    }
+    
+%FORMULAS%%TypeAbs%%
+}
