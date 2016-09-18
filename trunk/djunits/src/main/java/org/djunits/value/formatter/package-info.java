da7a59513cb2a57902eee79e44bc8d533cd39f8f
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2016-09-18 14:52:04 +0000

    License references to OTS changed into references to DJUNITS.

--- trunk/djunits/src/main/java/org/djunits/value/formatter/package-info.java
+++ trunk/djunits/src/main/java/org/djunits/value/formatter/package-info.java
@@ -2,7 +2,7 @@
  * Format values for display.
  * <p>
  * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
- * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * BSD-style license. See <a href="http://djunits.org/docs/license.html">DJUNITS License</a>.
  * <p>
  * @version $Revision$, $LastChangedDate$, by $Author$,
  *          initial version 11 sep. 2015 <br>
--- trunk/djunits/src/test/java/org/djunits/PrivateConstructorsTest.java
+++ trunk/djunits/src/test/java/org/djunits/PrivateConstructorsTest.java
@@ -13,7 +13,7 @@ import org.junit.Test;
  * throws of the statistics of our test coverage.
  * <p>
  * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
- * BSD-style license. See <a href="http://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * BSD-style license. See <a href="http://djunits.org/docs/license.html">DJUNITS License</a>.
  * <p>
  * @version $Revision$, $LastChangedDate$, by $Author$, initial
  *          version 27 sep. 2015 <br>
