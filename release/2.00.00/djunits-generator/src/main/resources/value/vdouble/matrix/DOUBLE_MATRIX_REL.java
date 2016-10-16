2fa4290404b9c31a19b1f5899aa8b4afa6bba4ef
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2016-10-16 15:35:33 +0000

    Version 2.00.00 of djunits, with a new typed structure.

---
+++ release/2.00.00/djunits-generator/src/main/resources/value/vdouble/matrix/DOUBLE_MATRIX_REL.java
@@ -0,0 +1,101 @@
+package org.djunits.value.vdouble.matrix;
+
+import java.util.List;
+import java.util.SortedMap;
+
+import org.djunits.unit.*;
+import org.djunits.value.StorageType;
+import org.djunits.value.ValueException;
+import org.djunits.value.vdouble.scalar.*;
+
+/**
+ * Immutable Double %Type%Matrix, a matrix of values with a %Type%Unit. 
+ * <p>
+ * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Oct 9, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class %Type%Matrix extends
+AbstractDoubleMatrixRel<%Type%Unit, %Type%Matrix, Mutable%Type%Matrix, %Type%>
+{
+    /** */
+    private static final long serialVersionUID = 20151109L;
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Matrix.
+     * @param values double[][]; the values of the entries in the new Relative Immutable Double %Type%Matrix
+     * @param unit U; the unit of the new Relative Immutable Double %Type%Matrix
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public %Type%Matrix(final double[][] values, final %Type%Unit unit, final StorageType storageType)
+        throws ValueException
+    {
+        super(values, unit, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Matrix.
+     * @param values DoubleScalar.Rel&lt;U&gt;[][]; the values of the entries in the new Relative Immutable Double
+     *            %Type%Matrix
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public %Type%Matrix(final %Type%[][] values, final StorageType storageType) throws ValueException
+    {
+        super(values, storageType);
+    }
+
+    /**
+     * @param data an internal data object
+     * @param unit the unit
+     */
+    %Type%Matrix(final DoubleMatrixData data, final %Type%Unit unit)
+    {
+        super(data, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final %Type%Matrix toDense()
+    {
+        return this.data.isDense() ? this : instantiateType(this.data.toDense(), getUnit());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final %Type%Matrix toSparse()
+    {
+        return this.data.isSparse() ? this : instantiateType(this.data.toSparse(), getUnit());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final %Type%Matrix instantiateType(final DoubleMatrixData dmd, final %Type%Unit unit)
+    {
+        return new %Type%Matrix(dmd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Mutable%Type%Matrix instantiateMutableType(final DoubleMatrixData dmd, final %Type%Unit unit)
+    {
+        return new Mutable%Type%Matrix(dmd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final %Type% instantiateScalar(final double value, final %Type%Unit unit)
+    {
+        return new %Type%(value, unit);
+    }
+
+
+    
+%FORMULAS%%Type%%
+}
+
