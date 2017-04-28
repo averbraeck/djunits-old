fcab35413e757f982e75c831bc069e798191b287
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2017-04-28 18:10:28 +0000

    Changes for DJUNITS 3.00.00. See OTS-319 in JIRA for explanation.
    Right now, absolute and separate variables use the same unit. E.g., a Time and a Duration both use a TimeUnit. In classes that have to enforce that all components are of unit TimeUnit, Duration as well as Time can be used. This is not desirable. Therefore we need to split the TimeUnit, AngleUnit, LengthUnit and TemperatureUnit into an absolute and a relative version.
    
    Absolute variables are variables with a relative offset to a defined value. E.g., 1 January of the year 1 can be used as the 'origin' for dates. Or 1 January 1970 for Unix times. For absolute time, this is called an Epoch (see https://en.wikipedia.org/wiki/Epoch_(reference_date)).
    This means that several instances of the TimeUnit need to be made, as there are many epochs. This can be nicely done with the OffsetLinearScale that gives a ratio to use when transforming one scale into another as well as an offset. When subtracting a Unix system time from a date that is relative to 0001-01-01T00:00 AD, we know through the scale that the start of the Unix system time is 719162 days after the year-1 epoch. Using the scale, addition and subtraction operations can be carried out without making further changes.. 
    
    An interesting unit is the TemperatureUnit. Here, we really messed up by mixing absolute temperature (including the offset) and relative temperature (which doesn't have an offset). We can say that another temperature is 10 degrees Celcius warmer than another temperature. Because of the offset, this was translated by the scale of the unit into 283.15 Kelvin... Ouch!
    
    So, the TemperatureUnit has a LinearScale whereas the AbsoluteTemperatureUnit has an OffsetLinearScale.
    
    the toAbs() and toRel() methods are removed. I don't understand how to translate 1-4-2017 at 12:00 to a relative value. The only thing that makes sense is to subtract a value (some kind offset) from the absolute value to get a relative value.
    
    Removed the methods to multiply and divide absolute values with absolute values. How can I multiply a position on this planet with another position on the planet? Or a time with another time? Just doesn't make sense, so removed the methods and the corresponding tests.

---
+++ release/3.00.00/djunits-generator/src/main/resources/value/vfloat/vector/FLOAT_VECTOR_AR_REL.java
@@ -0,0 +1,147 @@
+package org.djunits.value.vfloat.vector;
+
+import java.util.List;
+import java.util.SortedMap;
+
+import org.djunits.unit.*;
+import org.djunits.value.StorageType;
+import org.djunits.value.ValueException;
+import org.djunits.value.vfloat.scalar.*;
+
+/**
+ * Relative Float%TypeRel% Vector.
+ * <p>
+ * Copyright (c) 2013-2017 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://djunits.org/docs/license.html">DJUNITS License</a>.
+ * <p>
+ * $LastChangedDate: 2015-09-29 14:14:28 +0200 (Tue, 29 Sep 2015) $, @version $Revision: 73 $, by $Author: pknoppers $, initial
+ * version Sep 5, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class Float%TypeRel%Vector extends AbstractFloatVectorRel<%TypeRelUnit%, Float%TypeRel%Vector, MutableFloat%TypeRel%Vector, Float%TypeRel%>
+{
+    /** */
+    private static final long serialVersionUID = 20151006L;
+
+    /**
+     * Construct a new Relative Immutable Float%TypeRel%Vector.
+     * @param values float[]; the values of the entries in the new Relative Immutable Float%TypeRel%Vector
+     * @param unit U; the unit of the new Relative Immutable Float%TypeRel%Vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public Float%TypeRel%Vector(final float[] values, final %TypeRelUnit% unit, final StorageType storageType) throws ValueException
+    {
+        super(values, unit, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Float%TypeRel%Vector.
+     * @param values List; the values of the entries in the new Relative Immutable Float%TypeRel%Vector
+     * @param unit U; the unit of the new Relative Immutable Float%TypeRel%Vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public Float%TypeRel%Vector(final List<Float> values, final %TypeRelUnit% unit, final StorageType storageType) throws ValueException
+    {
+        super(values, unit, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Float%TypeRel%Vector.
+     * @param values FloatScalar.Rel&lt;U&gt;[]; the values of the entries in the new Relative Immutable Float%TypeRel%Vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public Float%TypeRel%Vector(final Float%TypeRel%[] values, final StorageType storageType) throws ValueException
+    {
+        super(values, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Float%TypeRel%Vector.
+     * @param values List; the values of the entries in the new Relative Immutable Float%TypeRel%Vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public Float%TypeRel%Vector(final List<Float%TypeRel%> values, final StorageType storageType) throws ValueException
+    {
+        super(values, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Float%TypeRel%Vector.
+     * @param values FloatScalar.Rel&lt;U&gt;[]; the values of the entries in the new Relative Sparse Mutable Float%TypeRel%Vector
+     * @param length the size of the vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public Float%TypeRel%Vector(final SortedMap<Integer, Float%TypeRel%> values, final int length, final StorageType storageType)
+        throws ValueException
+    {
+        super(values, length, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Float%TypeRel%Vector.
+     * @param values Map; the map of indexes to values of the Relative Sparse Mutable Float%TypeRel%Vector
+     * @param unit U; the unit of the new Relative Sparse Mutable Float%TypeRel%Vector
+     * @param length the size of the vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public Float%TypeRel%Vector(final SortedMap<Integer, Float> values, final %TypeRelUnit% unit, final int length,
+        final StorageType storageType) throws ValueException
+    {
+        super(values, unit, length, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Float%TypeRel%Vector.
+     * @param data an internal data object
+     * @param unit the unit
+     */
+    Float%TypeRel%Vector(final FloatVectorData data, final %TypeRelUnit% unit)
+    {
+        super(data, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Float%TypeRel%Vector instantiateType(final FloatVectorData dvd, final %TypeRelUnit% unit)
+    {
+        return new Float%TypeRel%Vector(dvd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final MutableFloat%TypeRel%Vector
+        instantiateMutableType(final FloatVectorData dvd, final %TypeRelUnit% unit)
+    {
+        return new MutableFloat%TypeRel%Vector(dvd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Float%TypeRel% instantiateScalar(final float value, final %TypeRelUnit% unit)
+    {
+        return new Float%TypeRel%(value, unit);
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    public final Float%TypeRel%Vector toDense()
+    {
+        return this.data.isDense() ? (Float%TypeRel%Vector) this : instantiateType(this.data.toDense(), getUnit());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final Float%TypeRel%Vector toSparse()
+    {
+        return this.data.isSparse() ? (Float%TypeRel%Vector) this : instantiateType(this.data.toSparse(), getUnit());
+    }
+    
+%FORMULAS%%TypeRel%%
+}
