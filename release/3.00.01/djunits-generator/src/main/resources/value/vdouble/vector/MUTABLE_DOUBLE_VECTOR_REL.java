4c790d048e49800636f2803736fb322795c19cf2
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2017-04-29 10:25:06 +0000

    Version 3.00.01.
    For DirectionUnit, EAST_RADIAN and EAST_DEGREE have been added.
    
    For TimeUnit, several BASE units have been added.
    
    In the TimeUnit, Time, and FloatTime classes a warning has been placed that indicates that precision is limited. For floatTime it states: Note that when the offset of a stored absolute Time becomes large, precision of a float might not be enough for the required resolution of a Time. A float has around 7 significant digits (23 bit mantissa). This means that when we need to have a float time that is precise to microseconds, the Time value should not go above 2^22 = 4.0E6. This is not enough to store Epoch values that are in the order of magnitude of 2E12 ms! So feeding System.TimeInMillis() to a FloatTime with TimeUnit.BASE as its unit is not having the required precision. At best, a FloatTime can store TimeUnit.BASE or TimeUnit.EPOCH values with real calendar values with a precision of several minutes.
    
    The GenerateUNITs has a replacement option that can insert the above precision warnings in the automatically generated Time and FloatTime (and other) classes.

---
+++ release/3.00.01/djunits-generator/src/main/resources/value/vdouble/vector/MUTABLE_DOUBLE_VECTOR_REL.java
@@ -0,0 +1,153 @@
+package org.djunits.value.vdouble.vector;
+
+import java.util.List;
+import java.util.SortedMap;
+
+import org.djunits.unit.*;
+import org.djunits.value.StorageType;
+import org.djunits.value.ValueException;
+import org.djunits.value.vdouble.scalar.*;
+
+/**
+ * Mutable Double %Type%Vector, a vector of values with a %Type%Unit. 
+ * <p>
+ * Copyright (c) 2013-2017 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Oct 9, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class Mutable%Type%Vector extends
+    AbstractMutableDoubleVectorRel<%Type%Unit, %Type%Vector, Mutable%Type%Vector, %Type%> %DIMLESS%
+{
+    /** */
+    private static final long serialVersionUID = 20151109L;
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Vector.
+     * @param values double[]; the values of the entries in the new Relative Immutable Double %Type%Vector
+     * @param unit U; the unit of the new Relative Immutable Double %Type%Vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public Mutable%Type%Vector(final double[] values, final %Type%Unit unit, final StorageType storageType)
+        throws ValueException
+    {
+        super(values, unit, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Vector.
+     * @param values List; the values of the entries in the new Relative Immutable Double %Type%Vector
+     * @param unit U; the unit of the new Relative Immutable Double %Type%Vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public Mutable%Type%Vector(final List<Double> values, final %Type%Unit unit, final StorageType storageType)
+        throws ValueException
+    {
+        super(values, unit, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Vector.
+     * @param values DoubleScalar.Rel&lt;U&gt;[]; the values of the entries in the new Relative Immutable Double
+     *            %Type%Vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public Mutable%Type%Vector(final %Type%[] values, final StorageType storageType) throws ValueException
+    {
+        super(values, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Vector.
+     * @param values List; the values of the entries in the new Relative Immutable Double %Type%Vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public Mutable%Type%Vector(final List<%Type%> values, final StorageType storageType) throws ValueException
+    {
+        super(values, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Vector.
+     * @param values DoubleScalar.Rel&lt;U&gt;[]; the values of the entries in the new Relative Sparse Mutable Double
+     *            %Type%Vector
+     * @param length the size of the vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public Mutable%Type%Vector(final SortedMap<Integer, %Type%> values, final int length, final StorageType storageType)
+        throws ValueException
+    {
+        super(values, length, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Double %Type%Vector.
+     * @param values Map; the map of indexes to values of the Relative Sparse Mutable Double %Type%Vector
+     * @param unit U; the unit of the new Relative Sparse Mutable Double %Type%Vector
+     * @param length the size of the vector
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public Mutable%Type%Vector(final SortedMap<Integer, Double> values, final %Type%Unit unit, final int length,
+        final StorageType storageType) throws ValueException
+    {
+        super(values, unit, length, storageType);
+    }
+    
+    /**
+     * @param data an internal data object
+     * @param unit the unit
+     */
+    Mutable%Type%Vector(final DoubleVectorData data, final %Type%Unit unit)
+    {
+        super(data, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final %Type%Vector instantiateType(final DoubleVectorData dvd, final %Type%Unit unit)
+    {
+        return new %Type%Vector(dvd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Mutable%Type%Vector instantiateMutableType(final DoubleVectorData dvd, final %Type%Unit unit)
+    {
+        return new Mutable%Type%Vector(dvd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final %Type% instantiateScalar(final double value, final %Type%Unit unit)
+    {
+        return new %Type%(value, unit);
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    public final Mutable%Type%Vector toDense()
+    {
+        return this.data.isDense() ? (Mutable%Type%Vector) this : instantiateMutableType(this.data.toDense(), getUnit());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final Mutable%Type%Vector toSparse()
+    {
+        return this.data.isSparse() ? (Mutable%Type%Vector) this : instantiateMutableType(this.data.toSparse(), getUnit());
+    }
+
+
+%FORMULAS%%Type%%
+
+}
+
