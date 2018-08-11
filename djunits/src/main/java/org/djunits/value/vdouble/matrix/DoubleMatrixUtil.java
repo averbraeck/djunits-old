eb478efbb6d5326d0e76c566384acc6a32e26cab
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2018-08-11 11:11:23 +0000

    For Scalar, FloatScalar, Vector, FloatVector, Matrix, and FloatMatrix, a generic instantiate method is added to a utility class (e.g., DoubleScalarUtil), which instantiates values based on the unit type it is provided with.

--- djunits/src/main/java/org/djunits/value/vdouble/matrix/DoubleMatrixUtil.java
+++ djunits/src/main/java/org/djunits/value/vdouble/matrix/DoubleMatrixUtil.java
@@ -257,7 +257,7 @@ public final class DoubleMatrixUtil
             value = (S) mvalue.divideBy(((MoneyPerVolumeUnit) displayUnit).getScaleFactor()).immutable();
         }
         else
-            value = (S) instantiate(si, displayUnit.getStandardUnit(), storageType);
+            value = (S) instantiateAnonymous(si, displayUnit.getStandardUnit(), storageType);
         ((AbstractDoubleMatrix) value).setDisplayUnit((Unit) displayUnit);
         return value;
     }
