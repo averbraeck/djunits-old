cee813630741789e8daef6c40e1a55230304c97a
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2018-08-11 11:22:26 +0000

    djunits updated to version 3.00.07

---
+++ release/3.00.07/djunits-generator/src/main/resources/value/vfloat/matrix/MUTABLE_FLOAT_MATRIX_REL.java
@@ -0,0 +1,101 @@
+package org.djunits.value.vfloat.matrix;
+
+import java.util.List;
+import java.util.SortedMap;
+
+import org.djunits.unit.*;
+import org.djunits.value.StorageType;
+import org.djunits.value.ValueException;
+import org.djunits.value.vfloat.scalar.*;
+
+/**
+ * Mutable Float%Type%Matrix, a matrix of values with a %Type%Unit. 
+ * <p>
+ * Copyright (c) 2013-2018 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Oct 9, 2015 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class MutableFloat%Type%Matrix extends
+    AbstractMutableFloatMatrixRel<%Type%Unit, Float%Type%Matrix, MutableFloat%Type%Matrix, Float%Type%> %DIMLESS%
+{
+    /** */
+    private static final long serialVersionUID = 20151109L;
+
+    /**
+     * Construct a new Relative Immutable Float%Type%Matrix.
+     * @param values float[][]; the values of the entries in the new Relative Immutable Float%Type%Matrix
+     * @param unit U; the unit of the new Relative Immutable Float%Type%Matrix
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values is null
+     */
+    public MutableFloat%Type%Matrix(final float[][] values, final %Type%Unit unit, final StorageType storageType)
+        throws ValueException
+    {
+        super(values, unit, storageType);
+    }
+
+    /**
+     * Construct a new Relative Immutable Float%Type%Matrix.
+     * @param values FloatScalar.Rel&lt;U&gt;[][]; the values of the entries in the new Relative Immutable Float
+     *            Float%Type%Matrix
+     * @param storageType the data type to use (e.g., DENSE or SPARSE)
+     * @throws ValueException when values has zero entries
+     */
+    public MutableFloat%Type%Matrix(final Float%Type%[][] values, final StorageType storageType) throws ValueException
+    {
+        super(values, storageType);
+    }
+
+    /**
+     * @param data an internal data object
+     * @param unit the unit
+     */
+    MutableFloat%Type%Matrix(final FloatMatrixData data, final %Type%Unit unit)
+    {
+        super(data, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final MutableFloat%Type%Matrix toDense()
+    {
+        return this.data.isDense() ? this : instantiateMutableType(this.data.toDense(), getUnit());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final MutableFloat%Type%Matrix toSparse()
+    {
+        return this.data.isSparse() ? this : instantiateMutableType(this.data.toSparse(), getUnit());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Float%Type%Matrix instantiateType(final FloatMatrixData fmd, final %Type%Unit unit)
+    {
+        return new Float%Type%Matrix(fmd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final MutableFloat%Type%Matrix instantiateMutableType(final FloatMatrixData fmd, final %Type%Unit unit)
+    {
+        return new MutableFloat%Type%Matrix(fmd, unit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected final Float%Type% instantiateScalar(final float value, final %Type%Unit unit)
+    {
+        return new Float%Type%(value, unit);
+    }
+    
+
+%FORMULAS%%Type%%
+
+}
+
