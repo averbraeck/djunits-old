24b4bc6907196f2660d9d57cc8dfb2cb9a30b5d2
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2019-10-18 23:01:21 +0000

    deployed version 4.00.02

---
+++ release/4.00.02/djunits/src/main/java/org/djunits/unit/EquivalentDoseUnit.java
@@ -0,0 +1,34 @@
+package org.djunits.unit;
+
+import org.djunits.unit.base.UnitBase;
+import org.djunits.unit.scale.IdentityScale;
+import org.djunits.unit.si.SIPrefixes;
+import org.djunits.unit.unitsystem.UnitSystem;
+
+/**
+ * The units of equivalent dose (of ionizing radiation).
+ * <p>
+ * Copyright (c) 2015-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
+ * <p>
+ * version May 15, 2014 <br>
+ * @author <a href="https://www.tudelft.nl/p.knoppers">Peter Knoppers</a>
+ */
+public class EquivalentDoseUnit extends Unit<EquivalentDoseUnit>
+{
+
+    /** */
+    private static final long serialVersionUID = 20190830;
+
+    /** The base, with "m2/s2" as the SI signature. */
+    public static final UnitBase<EquivalentDoseUnit> BASE = new UnitBase<>("m2/s2");
+
+    /** The SI unit for equivalent dose of ionizing radiation is Sievert. */
+    public static final EquivalentDoseUnit SI = new EquivalentDoseUnit()
+            .build(new Unit.Builder<EquivalentDoseUnit>().setUnitBase(BASE).setId("Sv").setName("sievert")
+                    .setUnitSystem(UnitSystem.SI_DERIVED).setSiPrefixes(SIPrefixes.UNIT).setScale(IdentityScale.SCALE));
+
+    /** Sievert. */
+    public static final EquivalentDoseUnit SIEVERT = SI;
+
+}
