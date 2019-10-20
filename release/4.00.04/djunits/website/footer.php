b8df9e4ff596f001051895c1e7b2ae4d31e4a335
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2019-10-20 01:37:25 +0000

    deployed version 4.00.04

---
+++ release/4.00.04/djunits/website/footer.php
@@ -0,0 +1,6 @@
+        <br id="endmain" />
+		<div id="footer">
+		  <hr />
+		  Copyright &copy; 2015-2019, Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved.
+		  <br>DJUNITS uses a BSD-style license. See <a href="http://djunits.org/docs/license.html">DJUNITS License</a>.
+		</div>
