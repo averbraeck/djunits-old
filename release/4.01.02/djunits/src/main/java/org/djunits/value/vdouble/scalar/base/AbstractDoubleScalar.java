019a3306d08d24c5943743e716c8d6ac1e021f01
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2019-11-10 16:16:24 +0000

    deployed djunits 4.01.02

---
+++ release/4.01.02/djunits/src/main/java/org/djunits/value/vdouble/scalar/base/AbstractDoubleScalar.java
@@ -0,0 +1,294 @@
+package org.djunits.value.vdouble.scalar.base;
+
+import org.djunits.unit.Unit;
+import org.djunits.value.Absolute;
+import org.djunits.value.AbstractScalar;
+import org.djunits.value.formatter.Format;
+import org.djunits.value.util.ValueUtil;
+
+/**
+ * The most basic abstract class for the DoubleScalar.
+ * <p>
+ * Copyright (c) 2013-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/staff/p.knoppers/">Peter Knoppers</a>
+ * @param <U> the unit
+ * @param <S> the type
+ */
+public abstract class AbstractDoubleScalar<U extends Unit<U>, S extends AbstractDoubleScalar<U, S>> extends AbstractScalar<U, S>
+        implements DoubleScalarInterface<U, S>
+{
+    /** */
+    private static final long serialVersionUID = 20161015L;
+
+    /** The value, stored in the standard SI unit. */
+    @SuppressWarnings("checkstyle:visibilitymodifier")
+    public final double si;
+
+    /**
+     * Construct a new AbstractDoubleScalar.
+     * @param unit U; the unit
+     * @param si double; the si value to store
+     */
+    AbstractDoubleScalar(final U unit, final double si)
+    {
+        super(unit);
+        this.si = si;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final double getSI()
+    {
+        return this.si;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final double getInUnit()
+    {
+        return ValueUtil.expressAsUnit(getSI(), getDisplayUnit());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final double getInUnit(final U targetUnit)
+    {
+        return ValueUtil.expressAsUnit(getSI(), targetUnit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean lt(final S o)
+    {
+        return this.getSI() < o.getSI();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean le(final S o)
+    {
+        return this.getSI() <= o.getSI();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean gt(final S o)
+    {
+        return this.getSI() > o.getSI();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean ge(final S o)
+    {
+        return this.getSI() >= o.getSI();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean eq(final S o)
+    {
+        return this.getSI() == o.getSI();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean ne(final S o)
+    {
+        return this.getSI() != o.getSI();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean lt0()
+    {
+        return this.getSI() < 0.0;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean le0()
+    {
+        return this.getSI() <= 0.0;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean gt0()
+    {
+        return this.getSI() > 0.0;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean ge0()
+    {
+        return this.getSI() >= 0.0;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean eq0()
+    {
+        return this.getSI() == 0.0;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final boolean ne0()
+    {
+        return this.getSI() != 0.0;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int compareTo(final S o)
+    {
+        return Double.compare(this.getSI(), o.getSI());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int intValue()
+    {
+        return (int) this.getSI();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public long longValue()
+    {
+        return (long) this.getSI();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public float floatValue()
+    {
+        return (float) this.getSI();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double doubleValue()
+    {
+        return this.getSI();
+    }
+
+    /**********************************************************************************/
+    /********************************* GENERIC METHODS ********************************/
+    /**********************************************************************************/
+
+    /** {@inheritDoc} */
+    @Override
+    protected Object clone() throws CloneNotSupportedException
+    {
+        return super.clone();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString()
+    {
+        return toString(getDisplayUnit(), false, true);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString(final U displayUnit)
+    {
+        return toString(displayUnit, false, true);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString(final boolean verbose, final boolean withUnit)
+    {
+        return toString(getDisplayUnit(), verbose, withUnit);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final String toString(final U displayUnit, final boolean verbose, final boolean withUnit)
+    {
+        StringBuffer buf = new StringBuffer();
+        if (verbose)
+        {
+            buf.append(this instanceof Absolute ? "Abs " : "Rel ");
+        }
+        double d = ValueUtil.expressAsUnit(getSI(), displayUnit);
+        buf.append(Format.format(d));
+        if (withUnit)
+        {
+            buf.append(" "); // Insert one space as prescribed by SI writing conventions
+            buf.append(displayUnit.getDefaultDisplayAbbreviation());
+        }
+        return buf.toString();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toTextualString()
+    {
+        return toTextualString(getDisplayUnit());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toTextualString(final U displayUnit)
+    {
+        double d = ValueUtil.expressAsUnit(getSI(), displayUnit);
+        return d + " " + displayUnit.getDefaultTextualAbbreviation();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toDisplayString()
+    {
+        return toDisplayString(getDisplayUnit());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toDisplayString(final U displayUnit)
+    {
+        double d = ValueUtil.expressAsUnit(getSI(), displayUnit);
+        return d + " " + displayUnit.getDefaultDisplayAbbreviation();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:designforextension")
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = getDisplayUnit().getStandardUnit().hashCode();
+        long temp;
+        temp = Double.doubleToLongBits(this.getSI());
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings({"checkstyle:designforextension", "checkstyle:needbraces", "unchecked"})
+    public boolean equals(final Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstractDoubleScalar<U, S> other = (AbstractDoubleScalar<U, S>) obj;
+        if (!getDisplayUnit().getStandardUnit().equals(other.getDisplayUnit().getStandardUnit()))
+            return false;
+        if (Double.doubleToLongBits(this.getSI()) != Double.doubleToLongBits(other.getSI()))
+            return false;
+        return true;
+    }
+
+}
