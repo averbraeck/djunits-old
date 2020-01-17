9bed3cc61db70b59bc28ed4369c4c0f83b77932f
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2020-01-17 15:42:30 +0000

    Committed version 4.01.04

---
+++ release/4.01.04/djunits-demo/src/main/java/org/djunits/demo/examples/VectorExample.java
@@ -0,0 +1,39 @@
+package org.djunits.demo.examples;
+
+import org.djunits.unit.LengthUnit;
+import org.djunits.unit.scale.Scale;
+import org.djunits.value.storage.StorageType;
+import org.djunits.value.vdouble.function.DoubleFunction;
+import org.djunits.value.vdouble.vector.LengthVector;
+import org.djunits.value.vdouble.vector.base.DoubleVector;
+
+public class VectorExample
+{
+
+    public static void main(final String[] args)
+    {
+        double[] data = new double[10];
+        for (int i = 0; i < data.length; i++)
+        {
+            data[i] = i * 0.8;
+        }
+        LengthVector lengthVector = DoubleVector.instantiate(data, LengthUnit.FOOT, StorageType.DENSE);
+        lengthVector = lengthVector.mutable();
+        System.out.println(lengthVector);
+        lengthVector.rint();
+        System.out.println(lengthVector);
+        
+        lengthVector = DoubleVector.instantiate(data, LengthUnit.FOOT, StorageType.DENSE);
+        lengthVector = lengthVector.mutable();
+        final Scale scale = lengthVector.getDisplayUnit().getScale();
+        lengthVector.assign(new DoubleFunction()
+        {
+            @Override
+            public double apply(final double value)
+            {
+                return scale.toStandardUnit(Math.rint(scale.fromStandardUnit(value)));
+            }
+        });
+        System.out.println(lengthVector);
+    }
+}
