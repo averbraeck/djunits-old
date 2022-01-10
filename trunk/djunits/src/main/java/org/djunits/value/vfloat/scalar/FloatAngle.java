1f26f73a4f3ab3b97807de903a910e6029d1f437
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2022-01-10 02:42:57 +0000

    Copyright notice extended to 2022. Javadoc updates.

--- trunk/djunits/src/main/java/org/djunits/value/vfloat/scalar/FloatAngle.java
+++ trunk/djunits/src/main/java/org/djunits/value/vfloat/scalar/FloatAngle.java
@@ -16,7 +16,7 @@ import org.djunits.value.vfloat.scalar.base.AbstractFloatScalarRelWithAbs;
 /**
  * Easy access methods for the FloatAngle FloatScalar.
  * <p>
- * Copyright (c) 2013-2021 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. <br>
+ * Copyright (c) 2013-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. <br>
  * All rights reserved. <br>
  * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
  * </p>
