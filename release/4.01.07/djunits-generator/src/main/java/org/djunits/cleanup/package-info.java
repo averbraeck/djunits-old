33ed1a4d4cbf9569ebab3f4b9e3958a54c9eb0b3
averbraeck <averbraeck@e6e90cb2-720e-4536-b696-02073a9ce678>
2022-01-10 11:18:14 +0000

    delete incomplete release 4.01.07

--- release/4.01.07/djunits-generator/src/main/java/org/djunits/cleanup/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Utility to clean the .classfile files for all projects; the excludes=** will be removed. <br>
- * <p>
- * Copyright (c) 2003-2022 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved.<br>
- * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
- * </p>
- * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
- */
-package org.djunits.cleanup;
